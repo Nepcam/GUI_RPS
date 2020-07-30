d6a9c1216a07028a3f47b7f4b3cea700a0f22fef
Nepcam <cameron.nepe@gmail.com>
2020-07-30 22:43:28 +1200

    Add project files.

---
+++ GUI_RPS/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace GUI_RPS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.playerLabel = new System.Windows.Forms.Label();
+            this.labelAnswer = new System.Windows.Forms.Label();
+            this.labelWelcome = new System.Windows.Forms.Label();
+            this.radioButtonRock = new System.Windows.Forms.RadioButton();
+            this.radioButtonPaper = new System.Windows.Forms.RadioButton();
+            this.radioButtonScissors = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Location = new System.Drawing.Point(578, 60);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(122, 23);
+            this.buttonPlay.TabIndex = 1;
+            this.buttonPlay.Text = "PLAY";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // playerLabel
+            // 
+            this.playerLabel.AutoSize = true;
+            this.playerLabel.Location = new System.Drawing.Point(165, 66);
+            this.playerLabel.Name = "playerLabel";
+            this.playerLabel.Size = new System.Drawing.Size(72, 13);
+            this.playerLabel.TabIndex = 2;
+            this.playerLabel.Text = "Player Choice";
+            // 
+            // labelAnswer
+            // 
+            this.labelAnswer.AutoSize = true;
+            this.labelAnswer.Location = new System.Drawing.Point(316, 125);
+            this.labelAnswer.Name = "labelAnswer";
+            this.labelAnswer.Size = new System.Drawing.Size(118, 13);
+            this.labelAnswer.TabIndex = 3;
+            this.labelAnswer.Text = "Welcome to RPS game";
+            // 
+            // labelWelcome
+            // 
+            this.labelWelcome.AutoSize = true;
+            this.labelWelcome.Location = new System.Drawing.Point(275, 23);
+            this.labelWelcome.Name = "labelWelcome";
+            this.labelWelcome.Size = new System.Drawing.Size(211, 13);
+            this.labelWelcome.TabIndex = 4;
+            this.labelWelcome.Text = "Please select PAPER SCISSORS or ROCK";
+            // 
+            // radioButtonRock
+            // 
+            this.radioButtonRock.AutoSize = true;
+            this.radioButtonRock.Location = new System.Drawing.Point(278, 66);
+            this.radioButtonRock.Name = "radioButtonRock";
+            this.radioButtonRock.Size = new System.Drawing.Size(55, 17);
+            this.radioButtonRock.TabIndex = 5;
+            this.radioButtonRock.TabStop = true;
+            this.radioButtonRock.Text = "ROCK";
+            this.radioButtonRock.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonPaper
+            // 
+            this.radioButtonPaper.AutoSize = true;
+            this.radioButtonPaper.Location = new System.Drawing.Point(373, 66);
+            this.radioButtonPaper.Name = "radioButtonPaper";
+            this.radioButtonPaper.Size = new System.Drawing.Size(61, 17);
+            this.radioButtonPaper.TabIndex = 6;
+            this.radioButtonPaper.TabStop = true;
+            this.radioButtonPaper.Text = "PAPER";
+            this.radioButtonPaper.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonScissors
+            // 
+            this.radioButtonScissors.AutoSize = true;
+            this.radioButtonScissors.Location = new System.Drawing.Point(469, 66);
+            this.radioButtonScissors.Name = "radioButtonScissors";
+            this.radioButtonScissors.Size = new System.Drawing.Size(79, 17);
+            this.radioButtonScissors.TabIndex = 7;
+            this.radioButtonScissors.TabStop = true;
+            this.radioButtonScissors.Text = "SCISSORS";
+            this.radioButtonScissors.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 199);
+            this.Controls.Add(this.radioButtonScissors);
+            this.Controls.Add(this.radioButtonPaper);
+            this.Controls.Add(this.radioButtonRock);
+            this.Controls.Add(this.labelWelcome);
+            this.Controls.Add(this.labelAnswer);
+            this.Controls.Add(this.playerLabel);
+            this.Controls.Add(this.buttonPlay);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.Label playerLabel;
+        private System.Windows.Forms.Label labelAnswer;
+        private System.Windows.Forms.Label labelWelcome;
+        private System.Windows.Forms.RadioButton radioButtonRock;
+        private System.Windows.Forms.RadioButton radioButtonPaper;
+        private System.Windows.Forms.RadioButton radioButtonScissors;
+    }
+}
+
---
+++ GUI_RPS/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI_RPS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static string answer = "";
+        static int count = 0;
+        static int count1 = 0;
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            while (answer != "NO")
+            {
+                //Player choice
+                labelAnswer.Text = "Select any one:\n1->ROCK\n2->PAPER\n3->SCISSOR";
+                //choices string array[] for computer choices
+                string[] choices = new string[3] { "ROCK", "PAPER", "SCISSOR" };
+                //declare random variable 
+                Random rand = new Random();
+                //declare variable and set it to a random selector with a range of values 
+                int number = rand.Next(0, 3);
+                
+                //string user = textBoxPlayerChoice.Text.ToUpper();
+                bool userRock = radioButtonRock.Checked;
+                bool userPaper = radioButtonPaper.Checked;
+                bool userScissors = radioButtonScissors.Checked;
+                labelAnswer.Text = "Computer:" + choices[number];
+
+                //if statements on choice combinations
+                if (userRock == radioButtonRock.Checked && choices[number] == "SCISSOR")
+                {
+                    labelAnswer.Text = "Player Wins!";
+                    count += 1;
+                }
+                else if (userRock == radioButtonRock.Checked && choices[number] == "PAPER")
+                {
+                    labelAnswer.Text = "Computer Wins!";
+                    count1 += 1;
+                }
+                else if (userPaper == radioButtonPaper.Checked && choices[number] == "ROCK")
+                {
+                    labelAnswer.Text = "Player Wins!";
+                    count += 1;
+                }
+                else if (userPaper == radioButtonPaper.Checked && choices[number] == "SCISSOR")
+                {
+                    labelAnswer.Text = "Computer Wins!";
+                    count1 += 1;
+                }
+                else if (userScissors == radioButtonScissors.Checked && choices[number] == "ROCK")
+                {
+                    labelAnswer.Text = "Computer Wins!";
+                    count1 += 1;
+                }
+                else if (userScissors == radioButtonScissors.Checked && choices[number] == "PAPER")
+                {
+                    labelAnswer.Text = "Player Wins!";
+                    count += 1;
+                }
+                else
+                {
+                    labelAnswer.Text = "Same choices";
+                }
+                labelAnswer.Text = "Play again?";
+
+
+                labelAnswer.Text = answer.ToUpper();
+
+                labelAnswer.Text = "----------------------------------";
+            }
+            labelAnswer.Text = "User wins " + count + " times";
+           
+            labelAnswer.Text = "Computer wins " + count1 + " times";
+
+        }
+    }
+}
